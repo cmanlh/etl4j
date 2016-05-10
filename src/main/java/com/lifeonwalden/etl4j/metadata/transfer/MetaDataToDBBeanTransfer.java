0d1cfc743cd0af3e8455dfb861e0c62831ea98ca
cmanlh <cmanlh@163.com>
2016-05-10 23:30:03 +0800

    add database infomation check

--- src/main/java/com/lifeonwalden/etl4j/metadata/transfer/MetaDataToDBBeanTransfer.java
+++ src/main/java/com/lifeonwalden/etl4j/metadata/transfer/MetaDataToDBBeanTransfer.java
@@ -17,56 +17,56 @@ import com.squareup.javapoet.TypeSpec;
 import com.squareup.javapoet.TypeSpec.Builder;
 
 public class MetaDataToDBBeanTransfer implements MetaDataTransfer {
-    private String packageName;
+	private String packageName;
 
-    private String className;
+	private String className;
 
-    public MetaDataToDBBeanTransfer(String packageName, String className) {
-        this.packageName = packageName;
-        this.className = className;
-    }
+	/**
+	 * if not pass className, will take the first column's table name as class
+	 * name
+	 * 
+	 * @param packageName
+	 * @param className
+	 */
+	public MetaDataToDBBeanTransfer(String packageName, String className) {
+		this.packageName = packageName;
+		this.className = className;
+	}
 
-    @Override
-    public String transfer(ResultSetMetaData rsmd) throws SQLException {
-        assert StringUtils.isNotBlank(packageName);
+	@Override
+	public String transfer(ResultSetMetaData rsmd) throws SQLException {
+		assert StringUtils.isNotBlank(packageName);
 
-        ClassName _className = ClassName.get(packageName.trim(), StringUtils.isBlank(className) ? rsmd.getTableName(1) : className.trim());
-        Builder beanBuilder =
-                TypeSpec.classBuilder(_className)
-                        .addModifiers(Modifier.PUBLIC)
-                        .addSuperinterface(Serializable.class)
-                        .addField(
-                                FieldSpec.builder(long.class, "serialVersionUID", Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
-                                        .initializer("$L", System.currentTimeMillis()).build());
-        int colCount = rsmd.getColumnCount();
-        for (int i = 1; i <= colCount; i++) {
-            String fieldName = rsmd.getColumnLabel(i);
-            String methodSubfix = StringUtils.capitalize(rsmd.getColumnLabel(i));
-            Class<?> fieldClass = null;
-            try {
-                fieldClass = Class.forName(rsmd.getColumnClassName(i));
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+		ClassName _className = ClassName.get(packageName.trim(), StringUtils.isBlank(className) ? rsmd.getTableName(1) : className.trim());
+		Builder beanBuilder = TypeSpec.classBuilder(_className).addModifiers(Modifier.PUBLIC).addSuperinterface(Serializable.class)
+				.addField(FieldSpec.builder(long.class, "serialVersionUID", Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+						.initializer("$L", System.currentTimeMillis()).build());
+		int colCount = rsmd.getColumnCount();
+		for (int i = 1; i <= colCount; i++) {
+			String fieldName = rsmd.getColumnLabel(i);
+			String methodSubfix = StringUtils.capitalize(rsmd.getColumnLabel(i));
+			Class<?> fieldClass = null;
+			try {
+				fieldClass = Class.forName(rsmd.getColumnClassName(i));
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			}
 
-            beanBuilder
-                    .addField(FieldSpec.builder(fieldClass, fieldName, Modifier.PRIVATE).build())
-                    .addMethod(
-                            MethodSpec.methodBuilder(StringUtils.join("set", methodSubfix)).addModifiers(Modifier.PUBLIC).returns(_className)
-                                    .addParameter(fieldClass, fieldName).addStatement("this.$L = $L", fieldName, fieldName)
-                                    .addStatement("return this").build())
-                    .addMethod(
-                            MethodSpec.methodBuilder(StringUtils.join("get", methodSubfix)).addModifiers(Modifier.PUBLIC).returns(fieldClass)
-                                    .addStatement("return this.$L", fieldName).build());
-        }
-        JavaFile javaFile = JavaFile.builder(packageName, beanBuilder.build()).build();
-        try {
-            javaFile.writeTo(System.out);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+			beanBuilder.addField(FieldSpec.builder(fieldClass, fieldName, Modifier.PRIVATE).build())
+					.addMethod(MethodSpec.methodBuilder(StringUtils.join("set", methodSubfix)).addModifiers(Modifier.PUBLIC).returns(_className)
+							.addParameter(fieldClass, fieldName).addStatement("this.$L = $L", fieldName, fieldName).addStatement("return this")
+							.build())
+					.addMethod(MethodSpec.methodBuilder(StringUtils.join("get", methodSubfix)).addModifiers(Modifier.PUBLIC).returns(fieldClass)
+							.addStatement("return this.$L", fieldName).build());
+		}
+		JavaFile javaFile = JavaFile.builder(packageName, beanBuilder.build()).build();
+		StringBuilder output = new StringBuilder();
+		try {
+			javaFile.writeTo(output);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 
-        return "";
-    }
+		return output.toString();
+	}
 }
--- src/test/java/com/lifeonwalden/etl4j/metadata/transfer/DatabaseInfoCheckTest.java
+++ src/test/java/com/lifeonwalden/etl4j/metadata/transfer/DatabaseInfoCheckTest.java
@@ -11,11 +11,10 @@ import java.sql.Statement;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MetaDataToDBBeanTransferTest {
+public class DatabaseInfoCheckTest {
 	private static final String CONNECTION_STRING = "jdbc:hsqldb:mem:testdb;shutdown=false";
 	private static final String USER_NAME = "SA";
 	private static final String PASSWORD = StringUtils.EMPTY;
@@ -68,69 +67,7 @@ public class MetaDataToDBBeanTransferTest {
 	}
 
 	@Test
-	public void transfer_emptyPackage() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
-
-		Assert.assertTrue(StringUtils.isNotBlank(
-				new MetaDataToDBBeanTransfer("", null).transfer(connection.createStatement().executeQuery("select * from \"User\"").getMetaData())));
-
-		connection.close();
-	}
-
-	@Test
-	public void transfer_nullPackage() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
-
-		try {
-			StringUtils.isNotBlank(new MetaDataToDBBeanTransfer(null, null)
-					.transfer(connection.createStatement().executeQuery("select * from \"User\"").getMetaData()));
-		} catch (NullPointerException e) {
-			Assert.assertTrue(true);
-			return;
-		} finally {
-			connection.close();
-		}
-
-		Assert.assertTrue(false);
-	}
-
-	@Test
-	public void transfer_withClassName() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
-
-		Assert.assertTrue(StringUtils.contains(new MetaDataToDBBeanTransfer("", "UserTest")
-				.transfer(connection.createStatement().executeQuery("select * from \"User\"").getMetaData()), "UserTest"));
-
-		connection.close();
-	}
-
-	@Test
-	public void transfer_notFullColumn() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
-		String classSrc = new MetaDataToDBBeanTransfer("", null)
-				.transfer(connection.createStatement().executeQuery("select \"id\", \"income\" from \"User\"").getMetaData());
-		Assert.assertFalse(StringUtils.contains(classSrc, "age"));
-		connection.close();
-	}
-
-	@Test
-	public void transfer_withJoin() throws SQLException, ClassNotFoundException {
-		Class.forName("org.hsqldb.jdbcDriver");
-		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
-		String classSrc = new MetaDataToDBBeanTransfer("", null).transfer(connection.createStatement()
-				.executeQuery("select u.\"id\", u.\"age\", b.\"name\" from \"Book\" b left join \"User\" u on (b.\"owner\" = u.\"id\")")
-				.getMetaData());
-		Assert.assertTrue(
-				StringUtils.contains(classSrc, "age") && StringUtils.contains(classSrc, "name") && !StringUtils.contains(classSrc, "income"));
-		connection.close();
-	}
-
-	@Test
-	public void transfer_test() throws SQLException, ClassNotFoundException {
+	public void tableInfo() throws SQLException, ClassNotFoundException {
 		Class.forName("org.hsqldb.jdbcDriver");
 		Connection connection = DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD);
 		DatabaseMetaData dbmd = connection.getMetaData();
